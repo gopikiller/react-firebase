aec2c5b5f6b296991cf1a1097901d86880238faf
Gopi <gopinath10007@gmail.com>
2022-10-29 13:57:57 +0530

    folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 function App() {
     const [count, setCount] = useState(0);
-
+    console.log(process.env.VITE_FIREBASE);
     return (
         <div className="App">
             <h1>Fire base App</h1>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,32 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [react()],
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd());
+    const envWithProcessPrefix = Object.entries(env).reduce(
+        (prev, [key, val]) => {
+            return {
+                ...prev,
+                ["process.env." + key]: `"${val}"`,
+            };
+        },
+        {}
+    );
+    return {
+        server: {
+            host: "localhost",
+            port: 3000,
+        },
+        preview: {
+            port: 8080,
+        },
+        define: envWithProcessPrefix,
+        plugins: [react()],
+        build: {
+            sourcemap: false,
+            outDir: "build",
+            minify: "esbuild",
+        },
+    };
 });
